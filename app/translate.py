744815c0367edb05834da45fd9628d7ef722b3fe
Kevin <kevinliu743@gmail.com>
2017-12-25 18:33:59 -0800

    enabled messages

--- app/translate.py
+++ app/translate.py
@@ -16,4 +16,4 @@ def translate(text, source_language, dest_language):
                      headers=auth)
     if r.status_code != 200:
         return _('Error: the translation service failed.')
-    return json.loads(r.content.decode('utf-8'))
+    return json.loads(r.content.decode('utf-8-sig'))
